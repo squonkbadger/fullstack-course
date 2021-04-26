d862b74ccde721ecaa51e5d6f1f3e58a26fbd8c0
squonkbadger <tatianatassi19@gmail.com>
2021-04-26 16:39:46 +0300

    2.14

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -2,7 +2,16 @@
 import axios from 'axios'
 import React, { useState, useEffect} from 'react'
 
-const CountryData = ({country}) => {
+const CountryData = ({country, api_key, setWeather, weather}) => {
+  const query = 'http://api.weatherstack.com/current?access_key='+api_key+'&query='+country[0].capital
+  useEffect(() => {
+    setWeather(null)
+    axios
+      .get(query)
+      .then(response => {
+        setWeather(response.data)
+      })
+  }, [query, setWeather])
   return (
   <div>
     <h2>
@@ -16,11 +25,23 @@ const CountryData = ({country}) => {
     <ul> {
       country[0].languages.map(language => <li key={language.name}>{language.name}</li>)
     }</ul>
-    <h3>
+    <h3> 
       Flag:
     </h3>
-    <img src={country[0].flag} alt="flag" width="300"/>
+    <img src={country[0].flag} alt="flag" width="100"/>
+    <h3>Weather:</h3>
+    {weather?<Weather weather = {weather}/>:null}
   </div>)
+}
+
+const Weather = ({weather}) => {
+  return (
+  <ul>
+    <li>Temperature: {weather.current.temperature} C</li>
+    <li><img src={weather.current.weather_icons[0]} alt="weather icon"/></li>
+    <li>Wind speed: {weather.current.wind_speed}</li>
+    <li>Wind direction: {weather.current.wind_dir}</li>
+  </ul>)
 }
 
 const CountryName = ({country, setNewSearch}) => {
@@ -31,14 +52,14 @@ const CountryName = ({country, setNewSearch}) => {
   </li>
   )}
 
-const Results = ({countriesToShow, setNewSearch}) => {
-  console.log(countriesToShow)
+const Results = ({countriesToShow, setNewSearch, api_key, setWeather, weather}) => {
+
   if (countriesToShow.length > 10 || countriesToShow.length === 0) {
     return (
     <div>Too many matches, specify another filter.</div>
   )
   } else if (countriesToShow.length === 1){
-    return (<div><CountryData country = {countriesToShow} /></div>)
+    return (<div><CountryData country = {countriesToShow} api_key = {api_key} setWeather = {setWeather} weather = {weather} /></div>)
   } else {
     return (
       <ul>{
@@ -49,27 +70,26 @@ const Results = ({countriesToShow, setNewSearch}) => {
 }
 
 function App() {
+  const api_key = process.env.REACT_APP_API_KEY
   const [ countries, setCountries] = useState([])
   
   const [ newSearch, setNewSearch ] = useState('')
-  
+  const [weather, setWeather] = useState(null)
   const countriesToShow = countries.filter(country =>
     country.name.toLowerCase().includes(newSearch.toLowerCase())===true)
-
+  
   const handleNewSearch = (event) => {
      setNewSearch(event.target.value)
   }
 
   useEffect(() => {
-    console.log('effect')
     axios
       .get('https://restcountries.eu/rest/v2/all')
       .then(response => {
-        console.log('promise fulfilled')
         setCountries(response.data)
       })
   }, [])
-  console.log(countriesToShow)
+ 
   return (
    <div>
      <h2>Country search</h2>
@@ -77,7 +97,7 @@ function App() {
        <input value= {newSearch} onChange = {handleNewSearch}/>
     </div>
     <h2>Results</h2>
-    <Results countriesToShow = {countriesToShow} setNewSearch = {setNewSearch}/>
+    <Results countriesToShow = {countriesToShow} setNewSearch = {setNewSearch} api_key={api_key} setWeather = {setWeather} weather ={weather}/>
   </div>
   )
 }
